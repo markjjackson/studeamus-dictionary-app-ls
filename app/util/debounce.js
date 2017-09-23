aad38027b6ec0a2bcf3c3ce6c060098ae01b6796
Mark Jackson <mark.j.jackson@me.com>
2017-09-23 21:07:16 +0200

    removed logs from debounce

--- app/util/debounce.js
+++ app/util/debounce.js
@@ -3,10 +3,8 @@
 const debounce = func => delay => {
   let debounceTimeout = null
   return function() {
-    console.log('test')
     let context = this
     let args = arguments
-    console.log(args)
     clearTimeout(debounceTimeout)
     debounceTimeout = setTimeout(() => {
       func.apply(context, args)
